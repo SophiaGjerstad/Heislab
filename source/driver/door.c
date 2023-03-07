2836183d4a83ee556edd8796ada2eaf746637b40
Sophia Gjerstad <sophia.gjerstad@ntnu.no>
2023-03-07 09:30:22 +0100

    Created header and c file for door class.

---
+++ source/driver/door.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdlib.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netdb.h>
+#include <stdio.h>
+//#include <pthread.h>
+
+/*Copied all includes from elevio.c file. 
+Don't know if all are needed.
+Therefore some commented out, but have them ready in this file in case.
+*/
+//#include elevio.h to access functions? How to avoid linker problems?
+#include "door.h"
