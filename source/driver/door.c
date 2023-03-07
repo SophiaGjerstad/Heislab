4566a840d404cc07e222805029b0836dd6405812
Martin Fredholm <martin313.fredholm@gmail.com>
2023-03-07 16:38:22 +0100

    Slettet (overflødige?) funksjoner

--- source/driver/door.c
+++ source/driver/door.c
@@ -11,33 +11,12 @@
 Don't know if all are needed.
 Therefore some commented out, but have them ready in this file in case.
 */
+
 Door* initializeDoor(void) {
     Door ElevatorDoor;
-    ElevatorDoor.doorChangeDoorStatus = doorChangeDoorStatus;
-    ElevatorDoor.doorChangeLightStatus = doorChangeLightStatus;
     ElevatorDoor.doorOpen = 0;
     ElevatorDoor.doorLampOn = 0;
-    Door * pointerElevatorDoor = &ElevatorDoor;
-    return pointerElevatorDoor;
-}
-//er ikke dette overflødig?
-
-
-void doorChangeDoorStatus(Door * pointerToDoor) {
-    //this should only change the door status. No logic
-    if (pointerToDoor->doorOpen) {
-        pointerToDoor->doorOpen = 0;
-    } else {
-        pointerToDoor->doorOpen = 1;
-    }
-}
-
-void doorChangeLightStatus(Door * pointerToDoor) {
-    if (pointerToDoor->doorLampOn) {
-        pointerToDoor->doorLampOn = 0;
-    } else {
-        pointerToDoor->doorLampOn = 1;
-    }
+    return &ElevatorDoor;
 }
 
 
--- source/driver/door.h
+++ source/driver/door.h
@@ -3,22 +3,11 @@
 
 typedef struct Door{
     bool doorOpen; 
-    bool doorLampOn; 
-    void (*doorChangeDoorStatus) (Door * pointerToDoor); //function pointers
-    void (*doorChangeLightStatus) (Door * pointerToDoor);
+    bool doorLampOn;
 }Door;
 
 
 Door* initializeDoor(void);
-/*this function initializes the struct Door by creating an instance of Door and
-inking the function pointers in the struct to the "set functions" doorChangeDoorStatus and
-doorChangeLightStatus. 
+/*this function initializes the struct Door by creating an instance of Door 
 It returns a pointer to the instance. Only use this one instance throughout the program!!*/
 
-//would get functions also be useful/necessary?
-
-void doorChangeDoorStatus(Door * pointerToDoor);
-//this function changes the door status to the opposite of what it was before. No other logic
-
-void doorChangeLightStatus(Door * pointerToDoor);
-//this function changes the light status to the opposite of what it was before. No other logic
