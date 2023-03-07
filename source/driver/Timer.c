fe3bc1f0e8ca886ad6698790d868d8a4b283d720
Sophia Gjerstad <sophia.gjerstad@ntnu.no>
2023-03-07 12:20:38 +0100

    Files for timer not necessary. Use nanosleep function in elevatorcontrol.

--- source/driver/Timer.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-//#include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netdb.h>
-//#include <assert.h>
-//#include <pthread.h>
-#include "Timer.h"
