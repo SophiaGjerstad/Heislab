171a7e52763af6ef831ccd3bc5d3136b35db65ff
Sophia Gjerstad <sophia.gjerstad@ntnu.no>
2023-03-07 19:09:06 +0100

    Removed a simple comment

--- source/driver/OrderHandler.h
+++ source/driver/OrderHandler.h
@@ -22,7 +22,6 @@ bool isThereRequestBelow(OrderHandlerStruct *inst,int currentFloor);//Checks if 
 OrderHandlerStruct* orderHandlerInitializer(void);
 //This function initializes a 4x3 matrix to hold requests for elevator floors and cabin buttons. 
 //The function should dynamically allocate memory and set all values in the matrix to zero.
-//[][] - how does this indexing work?
 //4 rows indicating each floor. Index[0] = first floor?
 //3 columns indicating order up, order down, order to specific floor number from cabin. 
 //Think about MINNELEKKASJE bruh
