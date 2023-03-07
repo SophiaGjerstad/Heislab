ffb3e2e91d33f213376b18a166305082dcf40b64
Martin Fredholm <martin313.fredholm@gmail.com>
2023-03-07 14:02:38 +0100

    Sartet arbeid på ElevatorControl

--- source/driver/ElevatorControl.h
+++ source/driver/ElevatorControl.h
@@ -1,3 +1,13 @@
 #pragma once
 
 //This file will contain functions that will be implemented in a state-machine based switch in main?
+
+typedef enum {Idle, EmergencyStop, MovingUp, MovingDown, Startup, ServicingFloor} State;
+
+typedef enum {Up, Down, none} ServiceMode;
+
+typedef struct ElevatorControl{
+    int currentFloor;
+    State currentState;
+    ServiceMode currentServiceingMode;
+}ElevatorControl;
