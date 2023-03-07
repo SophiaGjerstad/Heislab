6f4a1a32aaf8cecb67c1640d87c625e4c696c674
Martin Fredholm <martin313.fredholm@gmail.com>
2023-03-07 12:31:22 +0100

    Implementert headerfil og begynt å definere funksjoner

--- source/driver/OrderHandler.c
+++ source/driver/OrderHandler.c
@@ -1,18 +1,47 @@
 #include "OrderHandler.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 //#include <sys/types.h>
 //#include <sys/socket.h>
 //#include <netdb.h>
 //#include <assert.h>
 //#include <pthread.h>
 
-
-void orderHandlerInitializeHandler(void) {
+void orderHandlerInitializeHandler(void){
+    OrderHandlerStruct orderHandler={{{0,0,0},{0,0,0},{0,0,0},{0,0,0}}};
+    orderHandler.addToOrderHandlerMatrix = addToOrderHandlerMatrix;
+    orderHandler.deleteFromOrderHandlerMatrix = deleteFromOrderHandlerMatrix;
+    orderHandler.isThereRequestAbove = isThereRequestAbove;
+    orderHandler.isThereRequestBelow = isThereRequestBelow;
+    orderHandler.isThereRequestAtFloor = isThereRequestAtFloor;
     //dynamically allocate memory to 4x3 matrix. 
     //set values to 0. 
     //return a pointer to matrix somehow? We need to keep track of the initialized matrix and
     //be able to modify it later in the program. 
 }
 
+void addToOrderHandlerMatrix(OrderHandlerStruct *inst, int floor, RequestType typeOfRequest){
+    inst->orderMatrix[floor-1][typeOfRequest] = 1;
+}
+
+void deleteFromOrderHandlerMatrix(OrderHandlerStruct *inst, int floor, RequestType typeOfRequest){
+    inst->orderMatrix[floor-1][typeOfRequest] = 0;
+}
+
+bool isThereRequestAbove(OrderHandlerStruct *inst,int floor){
+
+}
+
+bool isThereRequestAbove(OrderHandlerStruct *inst,int floor){
+
+}
+
+int isThereRequestAtFloor(OrderHandlerStruct *inst,int floor){
+
+}
+
+bool isThereRequestBelow(OrderHandlerStruct *inst,int floor){
+
+}
 
--- source/driver/OrderHandler.h
+++ source/driver/OrderHandler.h
@@ -1,4 +1,27 @@
 #pragma once
+#include <stdbool.h>
+
+typedef enum {
+    Upp,
+    Down,
+    CabinRequest
+}RequestType;
+
+typedef struct OrderHandlerStruct
+{
+    bool orderMatrix [4][3];
+    void (*addToOrderHandlerMatrix)(OrderHandlerStruct *, int floor, RequestType typeOfRequest);
+    void (*deleteFromOrderHandlerMatrix)(OrderHandlerStruct *,int floor, RequestType typeOfRequest);
+    bool (*isThereRequestAbove)(OrderHandlerStruct *inst,int floor);
+    bool (*isThereRequestBelow)(OrderHandlerStruct *inst,int floor);
+    int (*isThereRequestAtFloor)(OrderHandlerStruct *inst,int floor);
+}OrderHandlerStruct;
+
+void addToOrderHandlerMatrix(OrderHandlerStruct *inst, int floor, RequestType typeOfRequest);//Adds an order of the specified type to the floor
+void deleteFromOrderHandlerMatrix(OrderHandlerStruct *inst,int floor, RequestType typeOfRequest);//Deletes an order of the specified type to the floor
+bool isThereRequestAbove(OrderHandlerStruct *inst,int floor);//Checks if there are any requests above the floor. 
+int isThereRequestAtFloor(OrderHandlerStruct *inst,int floor);//Checks if there are any requests above the floor. 
+bool isThereRequestBelow(OrderHandlerStruct *inst,int floor);//Checks if there are any requests above the floor. 
 
 void orderHandlerInitializeHandler(void);
 //This function initializes a 4x3 matrix to hold requests for elevator floors and cabin buttons. 
@@ -7,3 +30,4 @@ void orderHandlerInitializeHandler(void);
 //4 rows indicating each floor. Index[0] = first floor?
 //3 columns indicating order up, order down, order to specific floor number from cabin. 
 //Think about MINNELEKKASJE bruh
+
